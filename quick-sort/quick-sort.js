08a10d5d458a7e533d73f2def5640ebbdcf2d2c1
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2020-12-03 22:44:53 +0300

    add quick sort

---
+++ quick-sort/quick-sort.js
@@ -0,0 +1,23 @@
+const quickSort = (arr) => {
+    if (arr.length < 2) {
+        return arr
+    } else {
+        const pivot = arr[0]
+        const left = []
+        const right = []
+        for (let i = 1; i < arr.length; i++) {
+            if (pivot < arr[i]) {
+                left.push(arr[i])
+            } else {
+                right.push(arr[i])
+            }
+        }
+
+        return [...quickSort(left),pivot, ...quickSort(right)]
+    }
+
+}
+
+
+const sort = quickSort([9,3,4,1,8,5,6,7,33, 32,2,90])
+console.log(sort)
