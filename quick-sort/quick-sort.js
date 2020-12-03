95dac0f9c28adf6029684db8f00438d1b3555738
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2020-12-03 23:00:27 +0300

    add quick sort

--- quick-sort/quick-sort.js
+++ quick-sort/quick-sort.js
@@ -6,7 +6,7 @@ const quickSort = (arr) => {
         const left = []
         const right = []
         for (let i = 1; i < arr.length; i++) {
-            if (pivot < arr[i]) {
+            if (pivot > arr[i]) {
                 left.push(arr[i])
             } else {
                 right.push(arr[i])
